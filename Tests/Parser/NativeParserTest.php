f975cb5812d0fb443e2945f51e081109cdf3c982
Santiago San Martin <sanmartindev@gmail.com>
2025-09-07 18:50:47 -0300

    Use `>=` operator in `#[RequiresPhp]` attributes to allow compatible versions

--- Tests/Parser/NativeParserTest.php
+++ Tests/Parser/NativeParserTest.php
@@ -15,7 +15,7 @@ use PHPUnit\Framework\Attributes\RequiresPhp;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\HtmlSanitizer\Parser\NativeParser;
 
-#[RequiresPhp('8.4')]
+#[RequiresPhp('>=8.4')]
 class NativeParserTest extends TestCase
 {
     public function testParseValid()
